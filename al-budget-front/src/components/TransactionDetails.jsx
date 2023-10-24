0ac5c8b91cd4820d795fe140f90256c1acd22ff0
anickacodes <anickalewis@pursuit.org>
2023-10-24 16:59:01 -0400

    fetching ...

--- al-budget-front/src/components/TransactionDetails.jsx
+++ al-budget-front/src/components/TransactionDetails.jsx
@@ -1,16 +1,31 @@
-import '../styles/TransactionDetails.css'
+import { useState } from "react";
+import "../styles/TransactionDetails.css";
 
 const TransactionDetails = () => {
+  const API = import.meta.env.VITE_REACT_VAR_URL;
+  console.log(API)
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`${API}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((res) => setData(res))
+      .catch((err) => console.log("Error:", err));
+  }, []);
 
-    return (
-        <>
-        <h2> Our Add Transactions Details </h2>
-        {/* fetch */}
-        {/* useContext?? */}
-        {/* useStates */}
-        
-        </>
-    )
-}
+  return (
+    <>
+      <h2> Our Add Transactions Details </h2>
+      {/* fetch */}
+      {/* useContext?? */}
+      {/* useStates */}
+  
+    </>
+  );
+};
 
-export default TransactionDetails
+export default TransactionDetails;
