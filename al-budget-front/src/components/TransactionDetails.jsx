18bd357ffecef4efd3a8e3716798390922ab12e8
anickacodes <anickalewis@pursuit.org>
2023-10-24 21:51:42 -0400

    successful transaction obj fetched

--- al-budget-front/src/components/TransactionDetails.jsx
+++ al-budget-front/src/components/TransactionDetails.jsx
@@ -1,31 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/TransactionDetails.css";
 
 const TransactionDetails = () => {
   const API = import.meta.env.VITE_REACT_VAR_URL;
-  console.log(API)
-  const [data, setData] = useState(null);
+  console.log(`${API}/transactions`);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    fetch(`${API}`)
-      .then((res) => {
+    fetch(`${API}/transactions`)
+      .then(async (res) => {
         if (!res.ok) {
           throw new Error(`HTTP error! Status: ${res.status}`);
         }
-        return res.json();
+        // Parse the response data into JSON
+        const transactions = await res.json();
+
+        // Set the parsed JSON data in the component state
+        setData(transactions);
       })
-      .then((res) => setData(res))
       .catch((err) => console.log("Error:", err));
   }, []);
 
-  return (
-    <>
-      <h2> Our Add Transactions Details </h2>
-      {/* fetch */}
-      {/* useContext?? */}
-      {/* useStates */}
-  
-    </>
-  );
-};
+  // Check if the data variable is initialized before calling the map() function
+  if (data) {
+    return (
 
+      <table>
+        <thead>
+ <h2> Our Add Transactions Details </h2> 
+
+          <tr>
+            <th>Item Name</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>From</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {data.map((data) => (
+            <tr key={data.id}>
+              <td>{data.item_name}</td>
+              <td>${data.amount}</td>
+              <td>{data.date}</td>
+              <td>{data.from}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  } else {
+    // Return a loading indicator or something similar
+    return <p>Loading...</p>;
+  }
+};
 export default TransactionDetails;
+
+// {/* fetch */}
+// {/* useContext?? */}
+// {/* useStates */}
+
+  // headers: {
+      //   "Content-Type": "application/json",
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Cache-Control": "max-age=3600"
+      // }
